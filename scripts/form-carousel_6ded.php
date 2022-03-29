d1ff0945fed4d12f004115a4676870f62e0fbbd9
MarkThunderous <mkhoroshylov2@gmail.com>
2022-03-29 19:32:13 +0300

    111

---
+++ scripts/form-carousel_6ded.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'timeenergy',
+    'to' => 'mkhoroshylov2@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Наименование (фирма, предприятие...)',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Наименование (фирма, предприятие...)\' is required.'
+    )
+    ),
+    'select' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'select',
+    'required' => false,
+    'errors' => array(
+    'required' => 'Field \'select\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 3,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 4,
+    'type' => 'tel',
+    'label' => 'Телефон',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Телефон\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
